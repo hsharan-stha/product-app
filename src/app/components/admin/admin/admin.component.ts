8ec118bcf8dd909fcf4d33ab9aeaab74c619cde4
hsharan-stha <hsharan.shrestha@gmail.com>
2024-07-28 12:08:20 +0545

    feat: slicing started

--- src/app/components/admin/admin/admin.component.ts
+++ src/app/components/admin/admin/admin.component.ts
@@ -1,23 +1,38 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from "@angular/router";
+import {Component, HostBinding, OnInit} from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 import {AuthService} from "../../../service/auth/auth.service";
+import {NgOptimizedImage} from "@angular/common";
+import {UserInfo} from "../../../interface/UserInfo";
 
 @Component({
   selector: 'app-admin',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    RouterLink,
+    NgOptimizedImage,
+    RouterLinkActive
   ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit{
+
+  @HostBinding("class.display-block") className="display-block";
+
+  public userDetails:UserInfo;
 
   constructor(private authService:AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.userDetails=this.authService.getUserDetails();
   }
 
   public async logout():Promise<void>{
     await this.authService.logout();
   }
+
+
 
 }
