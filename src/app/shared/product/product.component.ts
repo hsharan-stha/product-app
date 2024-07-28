410dbc3326423e0de32d647c9748ec8bda7b6b4a
hsharan-stha <hsharan.shrestha@gmail.com>
2024-07-28 21:58:15 +0545

    feat : product list slicing

--- src/app/shared/product/product.component.ts
+++ src/app/shared/product/product.component.ts
@@ -1,33 +1,56 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
+import {AsyncPipe, NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
 import {FilterPipe} from "../../pipe/filter.pipe";
 import {InfiniteScrollDirective} from "ngx-infinite-scroll";
 import {Observable} from "rxjs";
 import {Product} from "../../interface/Product";
+import {AuthService} from "../../service/auth/auth.service";
 
 @Component({
   selector: 'app-product',
   standalone: true,
-    imports: [
-        AsyncPipe,
-        FilterPipe,
-        InfiniteScrollDirective,
-        NgForOf,
-        NgIf
-    ],
+  imports: [
+    AsyncPipe,
+    FilterPipe,
+    InfiniteScrollDirective,
+    NgForOf,
+    NgIf,
+    NgOptimizedImage
+  ],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
 
+  constructor(private authService:AuthService) {
+  }
+
   @Input() public products$:Observable<Product[]>;
   @Input() public searchText:string;
 
   @Output() public scrollEvent:EventEmitter<boolean>=new EventEmitter<boolean>();
+  @Output() public addToFavouriteEvent:EventEmitter<Product>=new EventEmitter<Product>();
 
 
   public scrollFn():void{
     this.scrollEvent.emit(true);
+  }
+
+  public getRandomImage(i:number):string{
+    return `https://picsum.photos/200/300?random=${i}`
+  }
+
+  public addToFavourite(data:Product){
+    this.addToFavouriteEvent.emit(data);
+  }
+
+  public checkIsAlreadyLiked(data:Product):boolean{
+
+    if(data?.isFavouriteOf && this.authService.getUserDetails()?.id) {
+      return <boolean>data.isFavouriteOf?.some(userId => userId === this.authService.getUserDetails()?.id)
+    }
+
+    return false
   }
 
 }
