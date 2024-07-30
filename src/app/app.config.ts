645f910f1517fdfa9637ba738e8b8309aba25c4b
Hari Sharan Shrestha <hari.shrestha@infodevelopers.local>
2024-07-30 17:12:19 +0545

    feat - interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,16 @@ import {APP_INITIALIZER, ApplicationConfig, provideZoneChangeDetection} from '@a
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient} from "@angular/common/http";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {httpInterceptor} from "./interceptor/http.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([httpInterceptor])
+    ),
     {
       provide:APP_INITIALIZER,
       useFactory:()=>{
